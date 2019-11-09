d7580ce7643c3c980aea8e87e3b54bf7cff6cd24
ajatdarojat45 <ajatdarojat45@gmail.com>
2019-11-09 09:00:30 +0700

    build

---
+++ build/precache-manifest.d88e43b94cec6a95e94b4392bc9c209a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e855c05e7c2157ff566dd059d27c1650",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9731456a21faa2e0b8c",
+    "url": "/static/css/main.c6f3bac8.chunk.css"
+  },
+  {
+    "revision": "64ca59075793db84cf72",
+    "url": "/static/js/2.a92dd24b.chunk.js"
+  },
+  {
+    "revision": "a9731456a21faa2e0b8c",
+    "url": "/static/js/main.b4dc7f3f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
